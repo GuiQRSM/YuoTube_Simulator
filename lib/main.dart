d66f590695eac58165556c4577f2881a2bc83786
GuiQRSM <endbeob15@gmail.com>
2021-12-27 14:37:31 -0300

    Instantiate in the 'home:,' attribute of 'MaterialApp()' the 'HomeFrame()' class that represents another screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/Home_Frame.dart';
 
 void main() {
 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeFrame(),
     ),
   );
 
