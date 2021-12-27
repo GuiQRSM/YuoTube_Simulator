106dd75e87559d4400b0065b544fe422e9954813
GuiQRSM <endbeob15@gmail.com>
2021-12-27 15:28:56 -0300

    Calls the attributes 'icon:,' and 'onPressed:,' inside the 'IconButton()' class, where 'icon:,' receives the 'Icon()' class

--- lib/Home_Frame.dart
+++ lib/Home_Frame.dart
@@ -20,7 +20,12 @@ class _HomeFrameState extends State<HomeFrame> {
         backgroundColor: pickColor,
         title: Text("YouTubeSimulator"),
         actions: <Widget>[
-          IconButton(),
+          IconButton(
+            icon: Icon(
+              Icons.videocam_rounded,
+            ),
+            onPressed: (){},
+          ),
         ],
       ),
       body: Container(),
