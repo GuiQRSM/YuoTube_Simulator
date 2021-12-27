5acfbc6395a0af05e84d7a9892c2235885fd923c
GuiQRSM <endbeob15@gmail.com>
2021-12-27 14:35:39 -0300

    Implements in the 'lib' folder the dart file called 'Home_Frame' with the import 'material.dart' and the structure 'stful' returning a class 'Scaffold()' already implemented

---
+++ lib/Home_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeFrame extends StatefulWidget {
+  const HomeFrame({Key? key}) : super(key: key);
+
+  @override
+  _HomeFrameState createState() => _HomeFrameState();
+}
+
+class _HomeFrameState extends State<HomeFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
