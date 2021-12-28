75f017952d9bf951a0ffa43da9dda376ed88d8cc
GuiQRSM <endbeob15@gmail.com>
2021-12-28 14:28:03 -0300

    Implement in the 'lib' folder a dart file called 'High Frame' with the import 'material.dart' and the structure 'stful' returning a class 'Scaffold()' already implemented

--- lib/InHigh_Frame.dart
+++ lib/InHigh_Frame.dart
@@ -7,9 +7,49 @@ class InHigh extends StatefulWidget {
   _InHighState createState() => _InHighState();
 }
 
+var pickColor = Color.fromRGBO(212, 25, 15, 1);
+
 class _InHighState extends State<InHigh> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: pickColor,
+          title: Image.asset(
+            "imgs/alternative_yt.png",
+            height: 30,
+            width: 100,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de pesquisa acionado!");
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.videocam_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de videoCAM acionado!");
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.account_circle_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de conta acionado!");
+              },
+            )
+          ],
+        ),
+    );
   }
 }
