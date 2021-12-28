4df4ad772ae97e9ebeb4cc9d63baba4b86ffecb6
GuiQRSM <endbeob15@gmail.com>
2021-12-28 17:27:15 -0300

    Calls in the 'Scaffold()' class which is returned by the 'stful' structure of 'Start' the attribute 'appBar:,' receiving the class 'AppBar()'

--- lib/Frames/Inicial_Frame.dart
+++ lib/Frames/Inicial_Frame.dart
@@ -7,9 +7,49 @@ class Inicio extends StatefulWidget {
   _InicioState createState() => _InicioState();
 }
 
+var pickColor = Color.fromRGBO(212, 25, 15, 1);
+
 class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: pickColor,
+          title: Image.asset(
+            "imgs/alternative_yt.png",
+            height: 30,
+            width: 100,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de pesquisa acionado!");
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.videocam_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de videoCAM acionado!");
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.account_circle_rounded,
+                color: Colors.black,
+              ),
+              onPressed: (){
+                print("Ícone de conta acionado!");
+              },
+            ),
+          ],
+        ),
+    );
   }
 }
