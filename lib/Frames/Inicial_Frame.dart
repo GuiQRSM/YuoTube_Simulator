c7bb29f6f6c64bc15debe16399083a3bc574131a
GuiQRSM <endbeob15@gmail.com>
2021-12-28 17:24:22 -0300

    Implements in the 'Frames' folder the dart file called 'Initial_Frames' with the import 'material.dart' and the 'stful' structure returned 'Scaffold()' already implemented

---
+++ lib/Frames/Inicial_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatefulWidget {
+  const Inicio({Key? key}) : super(key: key);
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
