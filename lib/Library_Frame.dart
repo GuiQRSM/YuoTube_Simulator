9c2b1ae9855284b434271bd86f79b48c9b645db8
GuiQRSM <endbeob15@gmail.com>
2021-12-28 14:47:17 -0300

    Implements in the 'Scaffold()' class which is returned by the 'stful' structure of 'Library' the attribute 'appBar:,' receiving the 'AppBar()' class

--- lib/Library_Frame.dart
+++ lib/Library_Frame.dart
@@ -7,9 +7,49 @@ class Library extends StatefulWidget {
   _LibraryState createState() => _LibraryState();
 }
 
+var pickColor = Color.fromRGBO(212, 25, 15, 1);
+
 class _LibraryState extends State<Library> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: pickColor,
+        title: Image.asset(
+          "imgs/alternative_yt.png",
+          height: 30,
+          width: 100,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search_rounded,
+              color: Colors.black,
+            ),
+            onPressed: (){
+              print("Ícone de pesquisa acionado!");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.videocam_rounded,
+              color: Colors.black,
+            ),
+            onPressed: (){
+              print("Ícone de videoCAM acionado!");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.account_circle_rounded,
+              color: Colors.black,
+            ),
+            onPressed: (){
+              print("Ícone de conta acionado!");
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
