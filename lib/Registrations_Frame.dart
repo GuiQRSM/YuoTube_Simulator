89aa0a91912203b3c9d2b6869ca9bd649bc13694
GuiQRSM <endbeob15@gmail.com>
2021-12-28 14:35:32 -0300

    Implement in the 'lib' folder the dart file called 'Registration_Frame' with the import 'material.dart' and the 'stful' structure of 'Registration' already implemented

---
+++ lib/Registrations_Frame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Registrations extends StatefulWidget {
+  const Registrations({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationsState createState() => _RegistrationsState();
+}
+
+var pickColor = Color.fromRGBO(212, 25, 15, 1);
+
+class _RegistrationsState extends State<Registrations> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
