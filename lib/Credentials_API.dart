e101e7eae9956581ba65fcccc2244677520c6872
GuiQRSM <endbeob15@gmail.com>
2022-01-03 13:55:36 -0300

    It implements within the dart file called 'Credentials_API' an import of a dependency called 'package:http/http.dart'

--- lib/Credentials_API.dart
+++ lib/Credentials_API.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+
 const KEY_YT_API = "AIzaSyCknUzdop22JPUPvkdt4ztQXdfzmBJhVKE";
 const ID_CHANNEL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
 const BASE_URL = "https://www.googleapis.com/youtube/v3/";
