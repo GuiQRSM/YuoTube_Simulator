574b6cdc3c6626cc093b9204fe999ff6d661ec5d
GuiQRSM <endbeob15@gmail.com>
2022-01-03 13:37:20 -0300

    Implement within the dart file called 'Credentials_API' a class called 'DataYT'

--- lib/Credentials_API.dart
+++ lib/Credentials_API.dart
@@ -1,3 +1,7 @@
 const KEY_YT_API = "AIzaSyCknUzdop22JPUPvkdt4ztQXdfzmBJhVKE";
 const ID_CHANNEL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
-const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+const BASE_URL = "https://www.googleapis.com/youtube/v3/";
+
+class DataYT {
+
+}
