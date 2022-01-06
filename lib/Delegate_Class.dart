c4ef9fd36abeb8df37f1e30c13bbda6ed7364c05
GuiQRSM <endbeob15@gmail.com>
2022-01-06 14:45:14 -0300

    Implement n class 'DelegateCustom' which 'extends' from the 'SearchDelegate' class four methods to overwrite

--- lib/Delegate_Class.dart
+++ lib/Delegate_Class.dart
@@ -2,4 +2,28 @@ import 'package:flutter/material.dart';
 
 class DelegateCustom extends SearchDelegate<String> {
 
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
 }
